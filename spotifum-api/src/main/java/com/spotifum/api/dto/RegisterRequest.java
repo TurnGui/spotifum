0a08118d0c88c09a43c6274d94f6a67e0c745fe5
Guilherme Duarte <guilhermefduarte2702@gmail.com>
2026-09-15 15:53:56 +0100

    feat: add JWT auth endpoints and global exception handler

---
+++ spotifum-api/src/main/java/com/spotifum/api/dto/RegisterRequest.java
@@ -0,0 +1,23 @@
+package com.spotifum.api.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterRequest {
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    private String name;
+
+    private String address;
+}
